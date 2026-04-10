87e645de7af963a26b5f27eec3f0861a2a7dc0b3
rafaeltorok <rafaeltorok@gmail.com>
2026-04-10 16:09:49 -0300

    Add exercise 10.8 from Part 10b

--- src/components/tabs/SignIn.jsx
+++ src/components/tabs/SignIn.jsx
@@ -1,5 +1,16 @@
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+// React Native
+import { 
+  View, 
+  Text, 
+  StyleSheet, 
+  TextInput, 
+  Button 
+} from "react-native";
 
+// Formik
+import { useFormik } from 'formik';
+
+// CSS Styles
 const styles = StyleSheet.create({
   container: {
     display: "flex",
@@ -20,21 +31,45 @@ const styles = StyleSheet.create({
   },
 });
 
+// Component
 export default function SignIn() {
+  // Create an instance of Formik
+  const formik = useFormik({
+    initialValues: ({
+      username: "",
+      password: ""
+    }),
+    onSubmit
+  });
+
+  // Submit button function
+  function onSubmit(values) {
+    console.log(`Username: ${values.username}; Password: ${values.password}`);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Sign-in</Text>
       <TextInput
+        id="username"
+        name="username"
         style={styles.inputField}
         placeholder="username"
+        value={formik.values.username}
+        onChangeText={formik.handleChange('username')}
       />
       <TextInput
+        id="password"
+        name="password"
         style={styles.inputField}
         placeholder="password"
+        value={formik.values.password}
+        onChangeText={formik.handleChange('password')}
         secureTextEntry={true}
       />
       <Button
-        title="Login"
+        title="Submit"
+        onPress={formik.handleSubmit}
       />
     </View>
   );
