c640db8dc7c5c566ac94f6347c38daaee9ebb611
rafaeltorok <rafaeltorok@gmail.com>
2026-04-16 16:50:46 -0300

    Remove unused import

--- client/src/components/repositories/RepositoryListContainer.jsx
+++ client/src/components/repositories/RepositoryListContainer.jsx
@@ -1,5 +1,5 @@
 // React Native
-import { FlatList, View, StyleSheet, Text } from "react-native";
+import { FlatList, View, StyleSheet } from "react-native";
 
 // Components
 import RepositoryItem from "./item/RepositoryItem";
