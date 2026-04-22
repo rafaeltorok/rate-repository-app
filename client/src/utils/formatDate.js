04603979c6eb47ce07dc1ef5f39533a9511bc750
rafaeltorok <rafaeltorok@gmail.com>
2026-04-22 14:48:10 -0300

    Update the date format for reviews - Implement the date-fns library to simplify the formatDate util

--- client/src/utils/formatDate.js
+++ client/src/utils/formatDate.js
@@ -1,13 +1,14 @@
-// Format the date into the format (MM-DD-YYYY)
+// Dependencies
+import { format } from "date-fns";
+
+// Format the date to DD MMM YYYY
 export default function formatDate(date) {
   // Transform the extended input date into an Date object
   const inputDate = new Date(date);
 
-  // Extract the year, month and day
-  const year = inputDate.getFullYear();
-  const month = String(inputDate.getMonth() + 1).padStart(2, "0");  // This method is zero based (January = 0)
-  const day = String(inputDate.getDate()).padStart(2, "0");
+  // Use the date-fns library to format
+  const outputDate = format(inputDate, "dd MMM yyyy");
 
   // Return the properly formatted date to be displayed on the Reviews section
-  return `${month}.${day}.${year}`;
+  return outputDate;
 }
