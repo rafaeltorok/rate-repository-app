fc7a91b70ba95a20819ae2c503762990548bd48d
rafaeltorok <rafaeltorok@gmail.com>
2026-04-22 14:50:03 -0300

    Improve comments on the util script

--- client/src/utils/formatDate.js
+++ client/src/utils/formatDate.js
@@ -1,14 +1,14 @@
 // Dependencies
 import { format } from "date-fns";
 
-// Format the date to DD MMM YYYY
+// Convert a string input to a valid date format
 export default function formatDate(date) {
   // Transform the extended input date into an Date object
   const inputDate = new Date(date);
 
-  // Use the date-fns library to format
+  // Use the date-fns library for the desired format
   const outputDate = format(inputDate, "dd MMM yyyy");
 
-  // Return the properly formatted date to be displayed on the Reviews section
+  // Return the proper date format to be displayed on the Reviews section
   return outputDate;
 }
